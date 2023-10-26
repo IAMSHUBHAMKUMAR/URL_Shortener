3657ea313f9439bc091bef85c6bb414e6a61a1ec
SHUBHAM KUMAR <shubhamku07@gmail.com>
2023-10-26 22:28:59 +0530

    Initial commit

---
+++ URL_Shortening/url_short.py
@@ -0,0 +1,36 @@
+from flask import Flask, requests,jsonify
+import hashlib
+
+app = Flask(__name__)
+url_database = {}
+
+def generate_short_url(long_url):
+    sha1_hash = hashlib.sha1(long_url.encode()).hexdigest()[:6]
+    return f"https://short.url/{sha1_hash}"
+
+@app.route('/shorten', methods = ['POST'])
+def shorten_url():
+    long_url = requests.form['long_url']
+    short_url = generate_short_url(long_url)
+    url_database[short_url] = {'long_url':long_url,'hits': 0}
+    return jsonify({'short_url': short_url})
+
+@app.route('/search', methods=['GET'])
+def search_url():
+    term = requests.args.get('term')
+    results = []
+    for short_url, data in url_database.items():
+        if term.lower() in data['long_url'].lower():
+            results.append({'title': data['long_url'], 'url': short_url})
+    return jsonify(results)
+
+@app.route('/<short_url>',methods = ['GET'])
+def redirect_to_long_url(short_url):
+    if short_url in url_database:
+        url_database[short_url]['hits'] +=1
+        return jsonify({'long_url': url_database[short_url]['long_url'], 'hits': url_database[short_url]['hits']})
+    else:
+        return jsonify({'error': 'Short URL not found'}),404
+    
+if __name__ == '__main__':
+    app.run(debug=True)
